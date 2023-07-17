056a838a0f639ce3ea68ebbb118eac64150e6bd6
Zulker Nien <zulkerb9b@gmail.com>
2023-07-17 23:27:33 +0600

    feat!: Navbar, Login and Landing added with routes

---
+++ src/app/components/landing.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Landing = () => {
+  return (
+    <div className="bg-white ">
+      <div className="mx-auto max-w-screen ">
+        <div className="relative isolate overflow-hidden bg-black px-6 pt-32 shadow-2xl lg:flex justify-center lg:gap-x-20 lg:px-24 lg:pt-24 h-screen">
+          <svg
+            viewBox="0 0 1024 1024"
+            className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+            aria-hidden="true"
+          >
+            <circle
+              cx="512"
+              cy="512"
+              r="512"
+              fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+              fill-opacity="0.7"
+            />
+            <defs>
+              <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                <stop stop-color="#7775D6" />
+                <stop offset="1" stop-color="#E935C1" />
+              </radialGradient>
+            </defs>
+          </svg>
+          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left ">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl pt-32">
+              Let us get started.
+            </h2>
+            <h2>Start by clicking the sign in button.</h2>
+            <p className="mt-6 text-lg leading-8 text-gray-300">
+              Ac euismod vel sit maecenas id pellentesque eu sed consectetur.
+              Malesuada adipiscing sagittis vel nulla.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <Link
+                href=""
+                className="lg:rounded-0 rounded-0 bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                Get Started
+              </Link>
+              <Link
+                href="#"
+                className="text-sm font-semibold leading-6 text-white"
+              >
+                Learn more <span aria-hidden="true">→</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
